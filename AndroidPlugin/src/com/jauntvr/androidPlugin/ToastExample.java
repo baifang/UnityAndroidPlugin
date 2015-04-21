80a0043fe5f835d718a68f747d543a293971aa58
baifang <baifang@gmail.com>
2015-04-21 11:51:55 -0700

    Added debug log on java.

--- AndroidPlugin/src/com/jauntvr/androidPlugin/ToastExample.java
+++ AndroidPlugin/src/com/jauntvr/androidPlugin/ToastExample.java
@@ -2,17 +2,20 @@ package com.jauntvr.androidPlugin;
  
 import android.content.Context;
 import android.widget.Toast;
- 
+import android.util.Log;
+
 public class ToastExample {
-    
+    private static final String TAG = "ToastExample";
     private Context context;
     private static ToastExample instance;
  
     public ToastExample() {
+        Log.d(TAG, "ToastExample");
         this.instance = this;
     }
  
     public static ToastExample instance() {
+        Log.d(TAG, "ToastExample::instance");
         if(instance == null) {
             instance = new ToastExample();
         }
@@ -20,10 +23,12 @@ public class ToastExample {
     }
  
     public void setContext(Context context) {
+        Log.d(TAG, "ToastExample::setContext");
         this.context = context;
     }
  
     public void showMessage(String message) {
+        Log.d(TAG, "ToastExample::showMessage");
         Toast.makeText(this.context, message, Toast.LENGTH_SHORT).show();
     }
  
